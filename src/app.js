e47ca87e84fb2b41f95fa7684bd8bf6b74f3a819
Zadnap <88957683+zadnap@users.noreply.github.com>
2025-11-28 13:54:21 +0700

    feat(routes): add router templates

--- src/app.js
+++ src/app.js
@@ -1,10 +1,19 @@
 const dotenv = require('dotenv');
 const express = require('express');
+const indexRouter = require('./routes/indexRouter');
+const bookRouter = require('./routes/bookRouter');
+const authorRouter = require('./routes/authorRouter');
+const publisherRouter = require('./routes/publisherRouter');
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.EXPRESS_PORT;
+
+app.use('/', indexRouter);
+app.use('/books', bookRouter);
+app.use('/authors', authorRouter);
+app.use('/publishers', publisherRouter);
 
 app.listen(PORT, (error) => {
   if (error) {
